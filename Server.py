3b3ba80a5c8d94c1f4c8ce4fe626711fafceaef9
mkaihan <80855938+mkaihan@users.noreply.github.com>
2022-11-22 22:07:00 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,32 @@
+import os
+
 class Server:
-    """ Server class for representing and manipulating servers. """
+    
 
     def __init__(self, server_ip):
         # TODO -
         self.server_ip = server_ip
+        self.ping()
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        print("The Server IP is", self.server_ip)
+        #Get the location from where the Python script is run
+        filepath = os.path.dirname(os.path.abspath(__file__))
+        #Set the working directory to the path where the python source code is
+        os.chdir(filepath)        
+        #pingresult = os.path.join(filepath,"pingResults__.txt")
+        #Create a text file Name to store the Ping results
+        pingresult = "pingResults__.txt"
+        pingText = 'ping ' + (self.server_ip) + ' > ' + pingresult         
+        print("Command sent to the OS system for the ping is: ", pingText)
+        response = os.system(pingText) 
+        #A response of 0 is returned for a sucessfull server response
+        f = open(pingresult, "r")
+        if (response == 0):            
+            print("Ping Successfull for the server", self.server_ip)            
+            print(f.read())
+        else:
+            print("Check the server - Unsuccessfull ping response for ", self.server_ip)
+            print(f.read())
+            
